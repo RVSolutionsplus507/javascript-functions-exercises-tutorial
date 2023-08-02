f9a4c749c0621e364cbffcaeebe861b10f75bde6
Roberto J. Vargas D <110059091+RVSolutionsplus507@users.noreply.github.com>
2023-08-02 21:25:00 +0000

    Ejercicio Completado RV

--- exercises/09-Array-Methods/app.js
+++ exercises/09-Array-Methods/app.js
@@ -1,6 +1,7 @@
 var names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
 
 function sortNames(arr){
-
+let filtro = arr.sort();
+return filtro;
 }
 console.log(sortNames(names));
--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,4 +1,12 @@
 // Your code goes here
+const rapid = (caracter) => {
+    let consonants = [];
+    for(let letra of caracter.toLowerCase()){
+        if(['a','e','i','o','u'].includes(letra) == false)
+            consonants.push(letra.toUpperCase())
+    }
+    return consonants.join('');
+};
 
 
 // Work above this line; do not change code below
